2a21394472c59c2d3d377d4cc03346d96c4e6fed
Amninder Brar <abrar14@rrc.ca>
2025-03-04 16:37:48 -0600

    added firebase in config and json file from firestore.

---
+++ config/firebase.ts
@@ -0,0 +1,12 @@
+import { initializeApp, cert, ServiceAccount } from "firebase-admin/app";
+
+import { getFirestore } from "firebase-admin/firestore";
+import  serviceAccount from "../firebase-service-account.ci.json";
+ 
+initializeApp({
+  credential: cert(serviceAccount as ServiceAccount),
+});
+ 
+
+const db = getFirestore();
+export { db };
